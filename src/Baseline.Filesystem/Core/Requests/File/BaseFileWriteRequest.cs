e8d5f6babf1dc8666270cc30cb3cf4ffa75a7440
Liam Symonds <liam@liamsymonds.com>
2022-10-31 14:57:47 +0000

    Resolves #342

---
+++ src/Baseline.Filesystem/Core/Requests/File/BaseFileWriteRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Baseline.Filesystem;
+
+/// <summary>
+/// A base class used for write requests. 
+/// </summary>
+public abstract class BaseFileWriteRequest<T> : BaseSingleFileRequest<T> where T : BaseFileWriteRequest<T>, new()
+{
+    /// <summary>
+    /// Gets or sets whether a <see cref="FileAlreadyExistsException"/> should be thrown if an attempt is made to
+    /// write to a path that already exists.
+    /// </summary>
+    public bool ThrowOnExistingFile { get; set; } = true;
+}
