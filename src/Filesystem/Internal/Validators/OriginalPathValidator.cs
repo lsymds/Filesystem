f910bae77aa55eb2898a32b057c77b7e8ff40038
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2025-02-03 22:57:10 +0000

    fix: Relax validation rules (#733)

--- src/Filesystem/Internal/Validators/OriginalPathValidator.cs
+++ src/Filesystem/Internal/Validators/OriginalPathValidator.cs
@@ -36,7 +36,7 @@ internal static class OriginalPathValidator
     /// <exception cref="PathContainsInvalidCharacterException" />
     private static void ThrowForInvalidCharacters(string path)
     {
-        const string invalidCharactersRegexString = @"[\*\""\\\[\]\;\|\,\<\>\'\$\£\%\^\(\)\+\=\!]";
+        const string invalidCharactersRegexString = @"[\*\""\\\[\]\;\|\,\<\>']";
 
         var invalidCharactersRegex = new Regex(invalidCharactersRegexString);
         if (invalidCharactersRegex.IsMatch(path))
